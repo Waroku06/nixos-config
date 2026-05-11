9e73d334f16ef5656c238f0a16e87c85628dff94
Waroku06 <178467046+Waroku06@users.noreply.github.com>
2026-05-11 12:14:05 +0800

    更新 fcitx5 与 fish

--- modules/features/fcitx5/def.nix
+++ modules/features/fcitx5/def.nix
@@ -6,14 +6,39 @@
       i18n.inputMethod = {
         enable = true;
         type = "fcitx5";
+        fcitx5.waylandFrontend = true;
         fcitx5.addons = with pkgs; [
-          fcitx5-rime # 中文
+          (fcitx5-rime.override {
+            rimeDataPkgs = [
+              rime-ice
+            ];
+          }) # 中文
           fcitx5-mozc # 日文
-          fcitx5-gtk # GTK 程序支持 (如 Firefox)
-          qt6Packages.fcitx5-chinese-addons # 额外中文组件（如拼音、拼写检查）
+          fcitx5-gtk # GTK 程序支持
+          qt6Packages.fcitx5-chinese-addons # 额外中文组件
           qt6Packages.fcitx5-configtool # 图形化配置工具
+          fcitx5-fluent # 主题皮肤
         ];
+        fcitx5.settings.inputMethod = {
+          GroupOrder."0" = "Default";
+          "Groups/0" = {
+            Name = "Default";
+            "Default Layout" = "us";
+            DefaultIM = "keyboard-us";
+          };
+          "Groups/0/Items/0".Name = "keyboard-us";
+          "Groups/0/Items/1".Name = "rime";
+          "Groups/0/Items/2".Name = "mozc";
+        };
       };
-      environment.variables.XMODIFIERS = "@im=fcitx";
     };
+  flake.homeModules.fcitx5 = _: {
+    home.file.".local/share/fcitx5/rime/default.custom.yaml".text = ''
+      patch:
+        __include: rime_ice_suggestion:/
+
+        schema_list:
+          - schema: rime_ice
+    '';
+  };
 }
